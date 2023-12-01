7f9cbc89e464987aeeb0fa0242849f71246ded0f
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2023-12-01 11:52:52 +0100

    Ajout class disque et test affichage livre

--- ProjetBibliothèque/Class/Articles.php
+++ ProjetBibliothèque/Class/Articles.php
@@ -1,11 +1,14 @@
 <?php
 
-class Livres {
+abstract class Articles {
     protected $titre;
-    protected $auteurs = array();
-    protected  $datePubli;
-    protected $editeur;
+    protected $datePubli;
     protected $genres = array();
+}
+
+class Livres extends Articles {
+    protected $auteurs = array();
+    protected $editeur;
 
     public function __construct($titre, $auteurs, $datePubli, $editeur, $genres) {
         $this->titre = $titre;
@@ -20,6 +23,32 @@ class Livres {
         foreach ($this->auteurs as $auteur) {
             echo $auteur .", ";
         }
-        echo "publié le " . $this->datePubli . " par " . $this->editeur . ". ";
+        echo "publié en " . $this->datePubli . " par " . $this->editeur . ". \nC'est un livre de :";
+        foreach ($this->genres as $genre) {
+            echo " " . $genre ;
+        }
+        echo ". ";
     }
 }
+
+class Disques extends Articles {
+    protected $artistes = array();
+
+    public function __construct($titre, $artistes, $datePubli, $genres) {
+        $this->titre = $titre;
+        $this->artistes = $artistes;
+        $this->datePubli = $datePubli;
+        $this->genres = $genres;
+    }
+
+    public function afficherDetails() {
+        echo "Le livre " . $this->titre . " écrit par : ";
+        foreach ($this->artistes as $artiste) {
+            echo $artiste .", ";
+        }
+        echo "publié le " . $this->datePubli . "\n";
+    }
+}
+
+$l1 = new Livres("SDA", ["JRR Tolkien"], 1975, "none", ["Fiction", "Heroic Fantasy"]);
+$l1->afficherDetails();
