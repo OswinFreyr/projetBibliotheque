53161dcf0ed7ebd9947c38e7bd60b6fb50710600
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2023-12-01 12:20:05 +0100

    Update Articles.php

--- ProjetBibliothèque/Class/Articles.php
+++ ProjetBibliothèque/Class/Articles.php
@@ -54,7 +54,7 @@ class Discs extends Articles {
     }
 }
 
-$l1 = new Books("SDA", ["JRR Tolkien"], 1975, "none", ["Fiction,", "Heroic Fantasy"]);
-$l1->afficherDetails();
-$d1 = new Discs("BG3 Soundtrack", ["Boris Slavov"], 2023, ["Jeux Videos"]);
-$d1->afficherDetails();
+// $l1 = new Books("SDA", ["JRR Tolkien"], 1975, "none", ["Fiction,", "Heroic Fantasy"]);
+// $l1->afficherDetails();
+// $d1 = new Discs("BG3 Soundtrack", ["Boris Slavov"], 2023, ["Jeux Videos"]);
+// $d1->afficherDetails();
